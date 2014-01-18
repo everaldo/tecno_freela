1276c7a1069be26b45cbe459ab4e7178d8d0b24d
Pedro Henrique (@phinfonet) <pedrohenriqueacruz@gmail.com>
2014-01-18 19:51:23 -0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TecnoFreela::Application.config.secret_key_base = '78ff412eed609f8485f5f41580bfc24b30599691a406a640a1a3e9fa7c913ee7728518cede2aa7f12a1f0a28870237489d0c291894edf281fac0e5c770ec23cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TecnoFreela::Application.config.session_store :cookie_store, key: '_tecno_freela_session'
