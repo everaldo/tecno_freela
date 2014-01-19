a847669f2a8f20bfd3ce56dba4dbef003ec0cb29
Pedro Henrique (@phinfonet) <pedrohenriqueacruz@gmail.com>
2014-01-19 16:34:19 -0200

    refact

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,64 @@
-class JobsController < ApplicationController
-  before_action :set_job, only: [:show]
+class JobsController < AplicationController
+  before_action :set_job, only: [:show, :edit, :update, :destroy]
+
+  # GET /jobs
+  # GET /jobs.json
+  def index
+    @jobs = Job.all
+  end
 
   # GET /jobs/1
   # GET /jobs/1.json
   def show
+  end
+
+  # GET /jobs/new
+  def new
+    @job = Job.new
+  end
+
+  # GET /jobs/1/edit
+  def edit
+  end
+
+  # POST /jobs
+  # POST /jobs.json
+  def create
+    @job = Job.new(job_params)
+
+    respond_to do |format|
+      if @job.save
+        format.html { redirect_to [:admin, @job], notice: 'Job was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @job }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: [:admin, @job.errors], status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /jobs/1
+  # PATCH/PUT /jobs/1.json
+  def update
+    respond_to do |format|
+      if @job.update(job_params)
+        format.html { redirect_to [:admin, @job], notice: 'Job was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: [:admin, @job.errors], status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /jobs/1
+  # DELETE /jobs/1.json
+  def destroy
+    @job.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_jobs_url }
+      format.json { head :no_content }
+    end
   end
 
   private
@@ -14,6 +69,6 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params[:job]
+      params.require(:job).permit(:job_name, :job_description, :job_apply, :jobs_category)
     end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -7,4 +7,9 @@ class Job
   field :job_name, type: String
   field :job_description, type: String
   field :job_apply, type: String
+  field :company_location, type: String
+  field :company_name, type: String
+  field :company_url, type: String
+  field :company_logo, type: String
+  field :company_email, type: String
 end
