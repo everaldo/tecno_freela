a50df048e187ccc72d9e72638c8d8da8b9dc1746
Everaldo Gomes <everaldo.gomes@gmail.com>
2014-08-29 15:38:39 +0000

    runned rake rails:update and updated settings - overwritings some files and customizing others

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-TecnoFreela::Application.config.session_store :cookie_store, key: '_tecno_freela_session'
+Rails.application.config.session_store :cookie_store, key: '_tecno_freela_session'
