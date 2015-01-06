a104d1f4f903bc8c8d2b30e33792c4c531fab317
Everaldo Gomes <everaldo.gomes@gmail.com>
2015-01-06 17:42:48 +0000

    commented out cookies serializer, due to a Mongoid/Devise bug - devise/pull/2882

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.action_dispatch.cookies_serializer = :json
+
+## Commented out the line below, due to a bug with Mongoid
+## https://github.com/plataformatec/devise/pull/2882
+#Rails.application.config.action_dispatch.cookies_serializer = :json
