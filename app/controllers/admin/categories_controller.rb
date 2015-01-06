02d3767c702c29ed53756f2684dbd5ffebb2ab04
Everaldo Gomes <everaldo.gomes@gmail.com>
2015-01-06 17:43:50 +0000

    renamed field from category_name to name

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -69,6 +69,6 @@ class Admin::CategoriesController < Admin::AdminController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
-      params.require(:category).permit(:category_name)
+      params.require(:category).permit(:name)
     end
 end
